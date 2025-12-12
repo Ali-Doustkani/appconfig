b6d6e1b0a0e0c94f6e2feabef8a82d3d1767903e
Ali Doustkani <ali.doostkany@gmail.com>
2025-12-12 21:45:11 +0100

    r extract test script

---
+++ .github/workflows/scripts/infra-test.sh
@@ -0,0 +1,31 @@
+#!/usr/bin/env bash
+set -euo pipefail
+
+groupName="$1"
+bicepOutputPath="$2"
+
+# read installed infra properties
+acrName=$(jq --raw-output ".acrName.value" $bicepOutputPath)
+appServiceName=$(jq --raw-output ".appServiceName.value" $bicepOutputPath)
+url="https://${appServiceName}.azurewebsites.net"
+echo "Testing app '$url'"
+
+echo "Importing nginx into registry '$acrName'"
+az acr import --name $acrName --source docker.io/library/nginx:1.29.4 --image nginx:testversion
+
+echo "Restarting app '$appServiceName'"
+az webapp restart --name $appServiceName --resource-group $groupName
+
+timeout=100 #timeout in seconds
+httpcode=$(curl --connect-timeout $timeout -i $url | head -n 1)
+if [[ "$httpcode" == *"200 OK"* ]]; then
+echo "PASSED -- 200OK"
+else 
+echo "Expected 200OK but received $httpcode"
+exit 1
+fi
+welcome=$(curl --connect-timeout $timeout $url | grep "Welcome to nginx")
+if [[ -z "$welcome" ]]; then
+echo "Expected website content"
+exit 1
+fi
