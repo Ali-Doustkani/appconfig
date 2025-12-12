f32a56468898a1afac1f225cfa560c889f881c20
Ali Doustkani <ali.doostkany@gmail.com>
2025-12-12 21:35:12 +0100

    add script file

---
+++ .github/workflows/scripts/deployinfra.sh
@@ -0,0 +1,13 @@
+#!/usr/bin/env bash
+set -euo pipefail
+
+$version="$1"
+$groupName="$2"
+
+echo "Deploying in resource group '$groupName'"
+az group create --name $groupName --location austriaeast --tag version=$version
+az deployment group create \
+--resource-group $groupName \
+--template-file infra/infra.bicep \
+--query "properties.outputs" \
+--output json > outputs.json
