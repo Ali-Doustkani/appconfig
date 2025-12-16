cd0ad0f100c9c690a55221b6fa44e780ac037152
Ali Doustkani <ali.doostkany@gmail.com>
2025-12-16 19:35:40 +0100

    extract http test bash

--- .github/workflows/scripts/infra-test.sh
+++ .github/workflows/scripts/infra-test.sh
@@ -7,8 +7,6 @@ bicepOutputPath="$2"
 # read installed infra properties
 acrName=$(jq --raw-output ".acrName.value" $bicepOutputPath)
 appServiceName=$(jq --raw-output ".appServiceName.value" $bicepOutputPath)
-url="https://${appServiceName}.azurewebsites.net"
-echo "Testing app '$url'"
 
 echo "Importing test image into registry '$acrName'"
 az acr import --name $acrName --source docker.io/alidoustkani/infratest:6 --image infratest:testversion
@@ -16,17 +14,6 @@ az acr import --name $acrName --source docker.io/alidoustkani/infratest:6 --imag
 echo "Restarting app '$appServiceName'"
 az webapp restart --name $appServiceName --resource-group $groupName
 
-# TEST CONTAINER RUN
-timeout=100 #timeout in seconds
-httpcode=$(curl --connect-timeout $timeout -i $url | head -n 1)
-if [[ "$httpcode" == *"200 OK"* ]]; then
-echo "PASSED -- 200OK"
-else 
-echo "Expected 200OK but received $httpcode"
-exit 1
-fi
-welcome=$(curl --connect-timeout $timeout $url | grep "HTTP OK")
-if [[ -z "$welcome" ]]; then
-echo "Expected website content"
-exit 1
-fi
+source ./infra-test-webapp.sh
+
+test_webapp $appServiceName
