c73451244c2d3e759bd081534bf0a4f20b987033
Ali Doustkani <ali.doostkany@gmail.com>
2025-12-17 22:29:14 +0100

    chore: clean up old testapp files

--- infra/init_container.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/sh
-
-# Get env vars in the Dockerfile to show up in the SSH session
-eval $(printenv | sed -n "s/^\([^=]\+\)=\(.*\)$/export \1=\2/p" | sed 's/"/\\\"/g' | sed '/=/s//="/' | sed 's/$/"/' >> /etc/profile)
-
-/usr/sbin/sshd
-mkdir -p /var/www/localhost/htdocs && echo "HTTP OK" > /var/www/localhost/htdocs/index.html
-lighttpd -D -f /etc/lighttpd/lighttpd.conf
