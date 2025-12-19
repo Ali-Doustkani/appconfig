622bec804208a4bc921df61bc0229257f5997567
Ali Doustkani <ali.doostkany@gmail.com>
2025-12-19 22:26:46 +0100

    cover set secret test

--- infra/tests/tests.py
+++ infra/tests/tests.py
@@ -14,3 +14,5 @@ assert "HTTP OK" in response.text, f"Expected response to contain 'HTTP OK' but 
 assert "App Config: infra_value" in response.text, f"Expected response to contain 'App Config: infra_value' but was '{response.text}'"
 
 assert "Key Vault: infra_value" in response.text, f"Expected response to contain 'Key Vault: infra_value' but was '{response.text}'"
+
+assert "Set Secret: denied" in response.text, f"Expected response to contain 'Set Secret: denied' but was '{response.text}'"    
